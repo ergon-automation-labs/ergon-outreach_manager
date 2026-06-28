77f570b1284c548c1c3d7e20db6d762c5f73899b
Abby Malson <august.malson@gmail.com>
2026-06-28 15:20:09 -0600

    bump: version 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyOutreachManager.MixProject do
   def project do
     [
       app: :bot_army_outreach_manager,
-      version: "0.1.1",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
